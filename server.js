98d40d5f187ba0e910879b9888394c0870ed8d7b
park-moen <pmj6516@naver.com>
2021-04-02 11:33:03 +0900

    conf: 장바구니 리펙토링 전 백업 작업

--- server.js
+++ server.js
@@ -12,15 +12,6 @@ const db = low(adapter);
 // Set default middlewares (logger, static, cors and no-cache)
 server.use(middlewares);
 
-// Add custom routes before JSON Server router
-server.delete('/userItem/completed', (req, res) => {
-  // lowdb를 사용해서 db.json에서 completed: true인 todo를 제거
-  db.get('userItem').remove({ checked: false }).write();
-
-  // todos를 응답
-  res.send(db.get('userItem').value());
-});
-
 // Use default router
 server.use(router);
 
