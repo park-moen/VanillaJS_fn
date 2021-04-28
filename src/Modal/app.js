db6d35c289749afcd39e89c95427541ee8d37b30
park-moen <pmj6516@naver.com>
2021-04-28 22:40:59 +0900

    modal function implement

--- src/Modal/app.js
+++ src/Modal/app.js
@@ -3,13 +3,15 @@ const closeModalButtons = document.querySelector('[data-close-button]');
 const overlay = document.getElementById('overlay');
 
 function openModal(modal) {
-  if (modal == null) return;
+  if (modal === null) return;
+
   modal.classList.add('active');
   overlay.classList.add('active');
 }
 
 function closeModal(modal) {
-  if (modal == null) return;
+  if (modal === null) return;
+
   modal.classList.remove('active');
   overlay.classList.remove('active');
 }
